58e10de05ef6e7e7d61a67f1b48591ad61260696
Freckie <freckie@frec.kr>
2018-10-18 17:34:23 +0900

    Fixed: comments

--- lab_04/lab_04_1.py
+++ lab_04/lab_04_1.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib as plt
 from torch.autograd import Variable
 
+# 데이터 로딩 (csv 타입)
 xy = np.loadtxt('./data/diabetes.csv.gz', delimiter=',', dtype=np.float32)
+# x 데이터와 y 데이터 로딩.
 x_data = Variable(torch.from_numpy(xy[:, 0:-1]))
 y_data = Variable(torch.from_numpy(xy[:, [-1]]))
-
 print(x_data.data.shape)
 print(y_data.data.shape)
 
@@ -14,43 +15,44 @@ print(y_data.data.shape)
 class Model(torch.nn.Module):
     def __init__(self):
         """
-        In the constructor we instantiate two nn.Linear module
+        모델 초기화 및 활성함수 설정.
         """
         super(Model, self).__init__()
+
+        # 3개의 layer를 세팅. (8->6->1)
         self.l1 = torch.nn.Linear(8, 6)
         self.l2 = torch.nn.Linear(6, 4)
         self.l3 = torch.nn.Linear(4, 1)
+
+        # 활성함수로 sigmoid 함수 설정.
         self.sigmoid = torch.nn.Sigmoid()
 
+    # 예측 함수.
     def forward(self, x):
-        """
-        In the forward function we accept a Variable of input data and we must return
-        a Variable of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Variables.
-        """
+        # y = sigmoid(sigmoid(sigmoid(x)))
         out1 = self.sigmoid(self.l1(x))
         out2 = self.sigmoid(self.l2(out1))
         y_pred = self.sigmoid(self.l3(out2))
         return y_pred
 
 
-# our model
 model = Model()
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
+# loss로 BCE 사용.
 criterion = torch.nn.BCELoss(size_average=True)
+# 최적화 방식으로 SGD 사용, 학습률=0.1.
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
-# Training loop
+# 학습 메인 루프.
 for epoch in range(100):
-    # Forward pass: Compute predicted y by passing x to the model
+    # x를 생성한 모델을 통해 예측.
     y_pred = model(x_data)
-    # Compute and print loss
+    # loss를 계산.
     loss = criterion(y_pred, y_data)
     print(epoch, loss.data[0])
-    # Zero gradients, perform a backward pass, and update the weights.
+    # gradient 초기화.
     optimizer.zero_grad()
+    # 오류 역전파.
     loss.backward()
+    # weight 갱신.
     optimizer.step()
