a9eaefd54f9c211d2cbf7ec4b0d0a71de0d72945
Freckie <freckie@frec.kr>
2018-11-22 14:53:23 +0900

    Fixed:

--- lab_07/lab_07_1.py
+++ lab_07/lab_07_1.py
@@ -11,17 +11,27 @@ from torchvision import datasets, transforms
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        self.l1 = nn.Linear(100 + 10, 256)
-        self.l2 = nn.Linear(256, 512)
-        self.l3 = nn.Linear(512, 1024)
-        self.l4 = nn.Linear(1024, 1 * 28 * 28)
+        self.map = nn.Sequential(
+            nn.Linear(100 + 10, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1 * 28 * 28),
+            nn.Tanh()
+        )
 
-    def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-        x = F.relu(self.l4(x))
-        return x
+    def forward(self, z, y):
+        try:
+            return self.map(torch.cat([z, y], 1))
+        except Exception as exc:
+            print(z.type())
+            print(z.shape)
+            print(y.type())
+            print(y.shape)
+            print(exc)
+            exit(1)
 
 
 # 분별기
@@ -39,7 +49,6 @@ class Discriminator(nn.Module):
             nn.Sigmoid()
         )
         
-
     def forward(self, x, y):
         # x의 shape를 batch_size * 28 * 28로 변경
         x = x.view(x.size(0), 28 * 28)
@@ -114,7 +123,7 @@ if __name__ == '__main__':
     # 학습
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = Variable(data), Variable(target)
-        z = Variable(torch.randn(100, 100))
+        z = Variable(torch.randn(100, 100).long())
 
         # 가짜 데이터 생성
         fake_images = model_G(z, target)
@@ -124,4 +133,19 @@ if __name__ == '__main__':
         fake_labels = Variable(torch.zeros(100))
 
         # 분별기(D) 학습
-        
+        loss_D, real_score, fake_score = train_D(model_D, data, real_labels, fake_images, fake_labels, target)
+
+        # batch_size * 100의 noise를 랜덤으로 추출
+        z = Variable(torch.randn(100, 100).long())
+
+        # 가짜 데이터 생성 후 분별
+        fake_images = model_G(z, target)
+        outputs = model_D(fake_images, target)
+
+        # 생성기(G) 학습
+        loss_G = train_G(model_G, outputs, real_labels, target)
+
+        # 중간 결과 출력
+        if batch_idx % 100 == 0:
+            logger.info('[{}/{} ({:.0f}%)] loss_D: {} / loss_G: {}'.format(
+                batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx * len(train_loader), loss_D.data[0], loss_G.data[0]))
